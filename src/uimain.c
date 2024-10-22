d2e723a0304f713526580c5ea0d62f8a8d1bc784
omardecai <earellano289@gmail.com>
2024-10-22 16:38:37 -0600

    update tokenize option

--- src/uimain.c
+++ src/uimain.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tokenizer.h"
+#include "history.h"
 
 int main()
 {
     int input = 1;
-    char str[50];
-    
+    char str[100];
+
     puts("Welcome to my tokenizer program!");
 
-    while (input != 0) {
+    while (input != 0)
+    {
         puts("Please Select an option: Tokenizer: 1, History: 2, Quit: 0");
         scanf("%d", &input);
-	getchar();
-        switch (input) {
-            case 1:
-	      puts("Tokenizer Selected");
-                printf(">");
-		fgets(str, sizeof(str), stdin);
-		printf("%s\n",str);
-                break;
-            case 2:
-                printf("History selected\n");
-                break;
-            case 0:
-                printf("Goodbye..\n");
-                break;
-            default:
-                printf("Invalid option, please try again.\n");
-                break;
+        getchar();
+        switch (input)
+        {
+        case 1:
+            puts("Tokenizer Selected");
+            printf(">");
+            fgets(str, sizeof(str), stdin);
+            char **tokens = tokenize(str);
+            print_tokens(tokens);
+            break;
+        case 2:
+            printf("History selected\n");
+            break;
+        case 0:
+            printf("Goodbye..\n");
+            break;
+        default:
+            printf("Invalid option, please try again.\n");
+            break;
         }
     }
 
